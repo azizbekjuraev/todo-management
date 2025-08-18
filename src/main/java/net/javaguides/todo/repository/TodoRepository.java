d812f9a8c3f6fa3f3415c4c7a8b48f3845de86a4
AZIZBEKJURAEV <ajuraev98@mail.ru>
2025-08-18 08:20:49 +0500

    learning java

---
+++ src/main/java/net/javaguides/todo/repository/TodoRepository.java
@@ -0,0 +1,9 @@
+package net.javaguides.todo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import net.javaguides.todo.entity.Todo;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+    
+}
---
+++ src/main/java/net/javaguides/todo/service/impl/TodoServiceImpl.java
@@ -0,0 +1,12 @@
+package net.javaguides.todo.service.impl;
+
+import net.javaguides.todo.dto.TodoDto;
+import net.javaguides.todo.service.TodoService;
+
+public class TodoServiceImpl implements TodoService {
+    @Override
+    public TodoDto addTodo(TodoDto todoDto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addTodo'");
+    }
+}
